25df0aafc7a18f79b4b5832b1369468ed295a9fe
DouglasSoares1203 <douglashardware13@gmail.com>
2022-11-23 21:41:29 -0300

    corrigindo alterações

--- src/components/ItemRepo/styles.js
+++ src/components/ItemRepo/styles.js
@@ -24,3 +24,35 @@ export const ItemContainer = styled.div`
         margin: 20px 0;
     }
 `
+export const ButtonsContainer = styled.div`
+    a.visitar{
+        color: #41487C;
+    }
+    a.remover {
+        color: #FF0000;
+        margin-top:20px;
+    }
+`
+
+export const ImgContainer = styled.div`
+    width: 10%;
+    display: inline-block;
+    img {
+        width: 50px;
+        heigth: 50px;
+    }
+`
+
+export const HeadContainer = styled.div`
+    width: auto;
+    display: inline-block;
+    h3 {
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+    p {
+        font-size:16px;
+        color: #FAFAFA70;
+        margin-bottom:20px;
+    }
+`
